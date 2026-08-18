1d2d490204f9998fa919e7044c3ef3a56176636e
Caiothez <caioventura3@gmail.com>
2026-08-18 20:15:43 -0300

    base para o frontend

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <section>
+        <div className = "flex items-center justify-center h-screen bg-zinc-800 text-shadow-white">
+          <p>To Querendo!!!</p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
+
