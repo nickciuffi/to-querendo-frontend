9a2efa0252cebcbc90a8012f15554fa56b80e8d0
Caiothez <caioventura3@gmail.com>
2026-08-25 21:12:48 -0300

    (ui): criado a base para usar shadcn no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
+// Importe o Card e subcomponentes com base no caminho onde salvou o arquivo
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+  CardAction
+} from "@/components/ui/card"
+
 function App() {
   return (
-    <>
+    <div className="flex items-center justify-center h-screen bg-amber-800">
       <section>
-        <div className = "flex items-center justify-center h-screen bg-zinc-800 text-shadow-white">
-          <p>To Querendo!!!</p>
-        </div>
+          <p className="text-blue-100 mb-5">To Querendo!!!</p>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Foda</CardTitle>
+          <CardDescription>Olha o milho</CardDescription>
+          <CardAction>
+            <button>R$ 10,00</button>
+          </CardAction>
+        </CardHeader>
+
+        <CardContent>
+          <p>Com manteiga e tudo mais</p>
+        </CardContent>
+
+        <CardFooter>
+          <button>Comprar</button>
+        </CardFooter>
+      </Card>
+
       </section>
-    </>
+    </div>
   )
 }
 
